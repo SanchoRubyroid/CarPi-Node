4257f6e5b4a7fbfc6d66b41ca3731dce01fbb0b8
Alexander Areshnikov <alex.areshnikov@plansource.com>
2016-04-28 15:38:07 -0600

    Implement captured vehicle

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ vehicleConnectionUtils.initializeVehiclesServer(io);
 io.of('/list').on('connection', function (socket) {
   console.log('List Connection: ' + socket.id);
 
-  socket.on('disconnect', () => {
-    console.log('Disconnected: ' + socket.id);
-  })
+  // socket.on('disconnect', () => {
+  //   console.log('Disconnected: ' + socket.id);
+  // })
 
   socket.emit('cars-list', vehicleConnectionUtils.vehicleNames());
 });
@@ -17,14 +17,25 @@ io.of('/control').on('connection', function (socket) {
   console.log('Control Connection: ' + socket.id);
 
   socket.on('controlled', (vehicle_name) => {
-    // TODO Check if already controlled
     socket.controlled_vehicle_name = vehicle_name
+
+    if(vehicleConnectionUtils.captured(vehicle_name))
+      throw new Error('Unexpected! Vehicle has been already captured.')
+
+    vehicleConnectionUtils.captureVehicleSocket(vehicle_name)
+    io.of('/list').emit('cars-list', vehicleConnectionUtils.vehicleNames())
+  })
+
+  socket.on('disconnect', () => {
+    vehicleConnectionUtils.releaseVehicleSocket(socket.controlled_vehicle_name)
+    io.of('/list').emit('cars-list', vehicleConnectionUtils.vehicleNames())
   })
 
   socket.on('car-control', (data) => {
-    //TODO Check if name set
-    var vehicle_socket = vehicleConnectionUtils.getSocketByName(socket.controlled_vehicle_name)
+    if(socket.controlled_vehicle_name === undefined)
+      throw new Error('Unexpected! controlled_vehicle_name should have been set.')
+
+    var vehicle_socket = vehicleConnectionUtils.getVehicleSocketByName(socket.controlled_vehicle_name)
     vehicle_socket.write(new Buffer(data))
-    //socket.controlled_vehicle_socket.write(new Buffer(data))
   })
 });
